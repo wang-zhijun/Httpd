294e3e1176147baa8813fbb95dfc9724daab3e08
Wang Zhijun <madfrogme@gmail.com>
2013-06-01 18:13:18 +0900

    keep working on web function

--- httpd.c
+++ httpd.c
@@ -30,7 +30,7 @@ struct {
 		{ "html", "text/html" },
 		{ 0, 0} };
 
-void log(int type, char *s1, char *s2, int num) {
+void log(int type, char *s1, char *s2, int num) { 
 	int fd;
 	char logbuffer[BUFSIZE*2];
 
@@ -40,6 +40,8 @@ void log(int type, char *s1, char *s2, int num) {
 			break;
 		case SORRY:
 			(void)sprintf(logbuffer, "<HTML><BODY><H1>nweb Web Server Sorry: %s %s</H1></BODY></HTML>\r\n", s1,s2);
+			(void)write(num,logbuffer,strlen(logbuffer));
+			(void)sprintf(lobbuffer,"SORRY: %s:%s",s1,s2);
 			break;
 		case LOG:
 			(void)sprintf(logbuffer, "INFO, %s:%s:%d", s1,s2,num);
@@ -54,6 +56,58 @@ void log(int type, char *s1, char *s2, int num) {
 	if (type == ERROR || type == SORRY) 
 		exit(3);
 } 
+
+/* child web server process, so we can exit on errors */
+void web(int fd, int hit) {
+	int j;
+	int buflen;
+	char *fstr;
+	long ret;
+	static char buffer[BUFSIZE];
+	
+	ret = read(fd, buffer, BUFSIZE);
+
+	if (ret == 0 || ret == -1) 	/* read failure, stop now */
+		log(SORRY, "failed to read browser request", fd);
+
+	if (ret > 0 && ret < BUFSIZE)
+		buffer[ret] = '\0'; /* terminate the buffer */
+	else
+		buffer[0] = '\0';
+
+	for (i = 0; i < ret; i++) 
+		if(buffer[i] == '\r' || buffer[i] == '\n')
+			buffer[i] = '*'; // WTF is this mean
+	
+	log(LOG, "request", buffer, hit);
+
+	/* now, let us theck the 'GET' or 'get' command */
+	if(strncmp(buffer, "GET ", 4) && strncmp(buffer, "get ", 4)) // no 'get ' or 'GET ' in buffer if true
+		log(SORRY, "Only simple GET operation supported", buffer, fd);
+
+
+	for (i = 4; i < BUFSIZE, i++) { /* null terminate after the second space to ignore extra stuff */
+		if (buffer[i] == ' ') {
+			buffer[i] = '\0'; // terminate the request after URL
+			break;
+		}
+	}
+
+	/* check for illegal parent directory use .. */
+	for (j = 0; j < i-1; j++) 
+		if(buffer[j] == '.' && buffer[j+1] == '.')
+			log(SORRY, "Parent directory (..) path names not supported", buffer, fd);
+
+
+	if(!strncmp(&buffer[0], "GET /\0", 6) || !strncmp(&buffer[0], "get /\0", 6) )
+		(void)strcpy(buffer,"GET /index.html");
+
+	buflen = strlen(buffer);
+	fstr = (char *)0;
+
+	for (i = 0; extension[i].ext != 0; i++) {
+
+	
 
 
 
